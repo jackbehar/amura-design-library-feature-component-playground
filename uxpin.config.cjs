0253c66fa41264e7632f37979e0e0fabbcf8578f
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2026-04-13 13:26:57 -0600

    update

--- uxpin.config.cjs
+++ uxpin.config.cjs
@@ -35,59 +35,9 @@ module.exports = {
     wrapper: 'src/uxpin/UXPinWrapper.tsx',
     webpackConfig: 'webpack.uxpin.config.cjs',
     settings: {
-      useAI: { name: 'Cloudscape Design System (Amazon)',
+      useAI: { name: 'Amura Design Library',
         documentationUrl: 'https://cloudscape.design/components/',
-        systemPrompt: `
-        All Component Rules:
-        - Do not include props that are functions or objects containing functions.
-        - Only use primitive props (string, number, boolean, null, undefined) and plain objects or arrays without any functions.
-        - Avoid inline arrow functions, callbacks, or event handlers in JSX.
-        Container Component Rules:
-        - props header and footer must always be defined.
-        - If no specific content is needed for header or footer, use an empty string ("").
-        - Do not omit header or footer under any circumstance.
-        Pagination Component Rules:
-        - omit ariaLabels property
-        Cards Component Rules:
-        - completely omit  cardDefinition property, it's generated automatically based on items property
-        Table Component Rules:
-        - columnDefinitions must be an array of objects:
-          {
-            id: string;
-            header: string;
-            sortingField?: string;
-            isRowHeader?: boolean;
-          }
-        
-        - items must be an array like:
-          {
-            name: string;
-            alt?: string;
-            description?: string;
-            type?: string;
-            size?: string;
-          }
-        - Do not pass functions in columnDefinitions (like cell or renderItem).
-        Wizard Component Rules:
-        - In the \`i18nStrings\` object, completely omit any properties that are functions, specifically:
-          - stepNumberLabel
-          - collapsedStepsLabel
-          - skipToButtonLabel
-        - Do not include these properties at all in the generated code, even as strings.
-        - Only include the other properties as usual.
-         Board Component Rules:
-         - items must be an array of objects:
-          {
-            id: string;
-            rowSpan?: number;
-            columnSpan?: number;
-            data: {
-                title: string;
-                content: React.ReactElement;
-            };
-          }
-          - you can only include empty and items properties at all in the generated code
-      ` },
+        systemPrompt: `` },
       useUXPinProps: true,
       useConvertingToUXPinClassic: true
     },
